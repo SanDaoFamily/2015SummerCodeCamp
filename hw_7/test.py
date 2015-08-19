399b0b02912b3d78cd686270c7722fd1ee321a55
sandaon <shen3546@gmail.com>
2015-08-19 11:40:07 +0800

    幹有夠機掰的找五百多年 操

--- hw_7/test.py
+++ hw_7/test.py
@@ -3,6 +3,18 @@ import os
 arraylist = []
 maillist = []
 
+# def add (order,maillist):
+# 	order = str(raw_input())
+# 	if order[0:4] == "add " :
+# 		for i in range (0, len(maillist), +1) :
+# 			if order[4:] == maillist[i].filename :
+# 				print "You silly ,fuck you !" 
+# 				add(order, maillist)	
+# 			else :
+# 				print order[8:]
+# 	else :
+# 		print "please add somthing "
+
 class Mail():
 	def __init__ (self,filename,ID,fromname,toname):
 		self.filename = filename
@@ -11,14 +23,24 @@ class Mail():
 		self.toname = toname
 	def __repr__(self):
 		return repr((self.filename, self.ID, self.toname, self.fromname))	
+
+
+
 while True :
+	
 	order = str(raw_input())
 	if order == "exit" :
 		break
-	if order[0:3] == "add" :
-		filename = order[4:]
-		# arraylist.append(filename)
+	if order[0:4] == "add " :
+		for i in range (0, len(maillist), +1) :
+			while order[4:] == maillist[i].filename :
+				print "You silly ,fuck you !" 
+				order = str(raw_input())
 
+		print order[8:]
+
+		filename = order[4:]
+		
 		for path,folders,files in os.walk("C:\Users\sandaon\Desktop\Python\hw_7\Test_data2") :
 	
 			a = open (os.path.join(path, filename),'r')
@@ -36,39 +58,45 @@ while True :
 				elif line.find("To: ") == 0 :
 					toname = line.replace("To: ","",1)
 
-					mail = Mail(filename,ID,toname,fromname)
+					mail = Mail(filename,ID,fromname,toname)
 					maillist.append(mail)
 
 
 
 	elif order[0:7] == "remove " :
 		removeID = order[7:]
+
+
 		for i in range (0, len(maillist),+1) :	
 
-			if removeID == maillist[i].ID[:-1]:
+			if removeID == maillist[i].ID[:-1] :
 				del maillist[i]
+				print removeID
 				break
-
-			elif removeID != maillist[i].ID[:-1]:
+			if i == len(maillist)-1 and removeID != maillist[i].ID[:-1] :
 				print "-"
-				break 
-			
 
-	elif order[0:2] == "-f" :
+
+			# elif removeID != maillist[len(maillist)-1].ID[:-1] :
+			# 	print "-"
+
+	elif order[0:8] == "query -f" :
 		findfrom = order.split('"')
 		findfromname = findfrom[1]
 		for i in range (0,len(maillist),+1) :
-			if findfromname == maillist[i].fromname[:-1] :
+			# print findfromname
+			# print type(findfromname)
+			# print maillist[i].fromname
+			# print type(maillist[i].fromname[:-1])
+			if  maillist[i].fromname[:-1] == findfromname:
 				print maillist[i].ID[:-1]
-				break
 
-	elif order[0:2] == "-t" :
+	elif order[0:8] == "query -t" :
 		findto = order.split('"')
 		findtoname = findto[1]
 		for i in range (0,len(maillist),+1) :
 			if findtoname == maillist[i].toname[:-1] :
 				print maillist[i].ID[:-1]
-				break
 
 
 	else :
